bba16504eae346dcd77811be9aa6a499afd34d70
Administrator <admin@example.com>
2021-06-01 00:23:59 +0200

    hhh7

--- src/base64.c
+++ src/base64.c
@@ -244,6 +244,10 @@ bool cjose_base64_decode(const char *input, size_t inlen, uint8_t **output, size
 }
 bool cjose_base64url_decode(const char *input, size_t inlen, uint8_t **output, size_t *outlen, cjose_err *err)
 {
-	if(!_decode(input, inlen, output, outlen, true, err))
+	if(!_decode(input, inlen, output, outlen, true, err)){
     return _decode(input, inlen, output, outlen, false, err);
+	}else{
+		return true;
+	}
+
 }
